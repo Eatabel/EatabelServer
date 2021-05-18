6c4d9153ea14278d12e0a1b39f693244cf5dbc5d
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 16:56:45 -0700

    Add Controller for Regular Lookup

--- server/controllers.js
+++ server/controllers.js
@@ -1,5 +1,17 @@
 const nutritionix = require('../nutritionix/config');
 
+const search = (req, res) => {
+  nutritionix.natural.search(req.params.query)
+    .then(result => {
+      res.status(200);
+      res.send({nutritionix: result});
+    })
+    .catch(() => {
+      res.status(404);
+      res.end();
+    });
+};
+
 const upcLookup = (req, res) => {
   nutritionix.item.byUpc(req.params.upc)
     .then(result => {
@@ -12,4 +24,5 @@ const upcLookup = (req, res) => {
     });
 };
 
+module.exports.search = search;
 module.exports.upcLookup = upcLookup;
--- server/index.js
+++ server/index.js
@@ -13,3 +13,4 @@ app.listen(port, () => {
 });
 
 app.get('/searchUPC/:upc', controllers.upcLookup);
+app.get('/search/:query', controllers.search);
